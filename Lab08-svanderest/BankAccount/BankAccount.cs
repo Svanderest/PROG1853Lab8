fda5b69c997ca61b3540356956f82ae928c4141b
Svanderest <35896486+Svanderest@users.noreply.github.com>
2019-02-06 22:45:22 -0500

    initial commit

---
+++ Lab08-svanderest/BankAccount/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//Sebastian van der Est
+namespace BankAccount
+{
+    class BankAccount
+    {
+        private string name;
+        protected decimal balance;
+        private int accNumber;
+        private DateTime open;
+        public const string BankName = "New Bank";
+
+        public string ClientName
+        {
+            get { return name; }
+            set
+            {
+                if (value == "")
+                    name = "Unknown";
+                else
+                    name = value;
+            }
+        }
+
+        public string Balance
+        {
+            get { return balance.ToString("C2"); }
+        }
+
+        public int AccountNumber
+        {
+            get { return accNumber; }
+        }
+
+        public string DateOpen
+        {
+            get { return open.ToLongDateString(); }
+        }
+
+        public BankAccount( int accountNumber, decimal startBalance )
+        {
+            this.ClientName = "";
+            this.balance = startBalance;
+            this.open = new DateTime();
+        }
+
+        public BankAccount( int accoutNumber, decimal startBalance, string nameOfClient )
+        {
+            this.ClientName = nameOfClient;
+            this.balance = startBalance;
+            this.open = new DateTime();
+        }
+
+        public void Deposit (decimal amount)
+        {
+            if (amount >= 0)
+                this.balance += amount;
+        }
+
+        public void Withdrawal (decimal amount)
+        {
+            if (amount >= 0 && amount <= this.balance)
+                this.balance -= amount;
+        }
+    }
+}
---
+++ Lab08-svanderest/BankAccount/FrmTestAccounts.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Sebastian van der Est
+namespace BankAccount
+{
+    public partial class FrmTestAccounts : Form
+    {
+        public FrmTestAccounts()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            // create a display string variable and initialize to an empty string
+            string display = "";
+            // create two bank account objects using both constructors - with valid data
+            BankAccount myAccount = new BankAccount(123, 100m);
+            BankAccount doeAccount = new BankAccount(234, 200m, "J Doe");
+            // test one of the objects - get accessors --> add output to display string
+            display += myAccount.ClientName + "\n" 
+                    + myAccount.DateOpen + "\n" 
+                    + myAccount.Balance + "\n" 
+                    + myAccount.AccountNumber + "\n\n";
+            // test one of the objects - set accessors  --> add output to display string
+            doeAccount.ClientName = "John Doe";
+            display += doeAccount.ClientName + "\n"
+                    + doeAccount.DateOpen + "\n"
+                    + doeAccount.Balance + "\n"
+                    + doeAccount.AccountNumber + "\n\n";
+            // test one of the objects - methods  --> add output to display string
+            doeAccount.Withdrawal(100m);
+            display += doeAccount.Balance + "\n\n";
+            // show display string to user - use bank name in caption of message box
+            MessageBox.Show(display, BankAccount.BankName);
+            /*****************************Next Test*********************************/
+            // reset display string to empty string
+            display = "";
+            // create one account using the 3 parameter constructor - invalid name
+            BankAccount testAccount = new BankAccount(555, 27m, "");
+            // use get accessor to check name  --> add output to display string
+            display += testAccount.ClientName + "\n";
+            // use set accessor to set invalid name  --> add output to display string
+            testAccount.ClientName = "";
+            display += testAccount.ClientName + "\n";
+            // use set accessor to set valid name  --> add output to display string
+            testAccount.ClientName = "John Doe";
+            display += testAccount.ClientName + "\n";
+            // show display sgtring to user - use bank name in caption of message box
+            MessageBox.Show(display, BankAccount.BankName);
+        }
+    }
+}
